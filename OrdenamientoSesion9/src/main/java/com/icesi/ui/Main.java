1de49e2a39a3b400de013d80bc343c1db16d054a
Alvaro <roa.alvarez7alvaro.jose@gmail.com>
2025-12-07 14:37:34 -0500

    Terminado

--- OrdenamientoSesion9/src/main/java/com/icesi/ui/Main.java
+++ OrdenamientoSesion9/src/main/java/com/icesi/ui/Main.java
@@ -1,4 +1,40 @@
 package com.icesi.ui;
 
+import com.icesi.model.ComputersSale;
+
+import java.util.ArrayList;
+
 public class Main {
+    //Relationships or Atributes (global variables)
+    private ComputersSale computersSale = new ComputersSale();
+
+    //Methods
+    public void printArrays() {
+        ArrayList<ArrayList<String>> sorts = new ArrayList<>();
+        computersSale.generateRandomComputers(100);
+        computersSale.sortByBrand();
+        sorts.add(computersSale.getComputersInfo());
+        computersSale.sortByProcessing();
+        sorts.add(computersSale.getComputersInfo());
+        computersSale.sortByBrandAndOS();
+        sorts.add(computersSale.getComputersInfo());
+        computersSale.sortByRam();
+        sorts.add(computersSale.getComputersInfo());
+        computersSale.sortByBrandReverse();
+        sorts.add(computersSale.getComputersInfo());
+        for(ArrayList<String> s: sorts){
+            for(String s1 : s){
+                System.out.println(s1);
+            }
+        }
+    }
+
+    public Main(){
+
+    }
+
+    public static  void main(String[] args) {
+        Main main = new Main();
+        main.printArrays();
+    }
 }
